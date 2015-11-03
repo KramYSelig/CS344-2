3400b210cf4dcbc861d2dc968b954c35f8ec106c
Mark Giles <gilesm@flip2.engr.oregonstate.edu>
2015-11-02 17:29:30 -0800

    Final submission, working solution

--- gilesm.adventure.c
+++ gilesm.adventure.c
@@ -43,10 +43,9 @@ struct Game {
 	int stepCount;						// tracks number of steps taken
 	int processID;						// process ID of current game
 	char dirPath[512];					// path of directory for files
+	char stepFileName[512];				// name of step history file 
 	int startRoomIndex;					// room list index of starting room
 	int endRoomIndex;					// room list index of ending room
-	char stepList[10][50];				// list of room names for step history
-	int stepListSize;
 };
 
 // takes a game structure, a room index, and adds a connection to that room
@@ -69,11 +68,11 @@ void initGame(struct Game *currentGame);
 void playGame(struct Game *currentGame);
 
 int main() {
+	// initialize game structure and allocate memory
 	struct Game *currentGame;
 	currentGame = (struct Game *)malloc(sizeof(struct Game));
 	// initialize seed for random number generator
 	srand (time(NULL));
-	
 	// initialize the game attributes, room name list, and directory path
 	initGame(currentGame);
 	// assign room names, room connections, and save room files to directory
@@ -83,7 +82,7 @@ int main() {
     // display congratulations, step count, and step history path to the user
 	displayGameResults(currentGame);
 	// clean game data
-	deleteTempFiles(currentGame);
+	remove(currentGame->stepFileName);
 	free(currentGame); 
     
 	// exit with value 0
@@ -100,12 +99,10 @@ void buildGame(struct Game *currentGame) {
 		i = 0,					// iterator for control structures
 		j = 0,					// iterator for control structures
 		numConnToAdd;			// number of connections selected for this room
-	char *fileName = malloc(sizeof(char) * 10);
-	// stores random name selection from list
-	char *roomName = malloc(sizeof(char) * 50);
+	char *fileName = malloc(sizeof(char) * 10),
+		 *roomName = malloc(sizeof(char) * 50);
 		
-	
-		// create room files while assigning name, type, and connections
+	// create room files while assigning name, type, and connections
 	for (i = 0; i < 7; i++) {
 		// select random name from name list
 		getRandomName(currentGame, roomName);
@@ -127,7 +124,6 @@ void buildGame(struct Game *currentGame) {
 		}
 	}
 
-		
 	// populate room files with description/information
 	for (i = 0; i < 7; i++) {
 		writeRoomFile(currentGame, i);
@@ -137,7 +133,8 @@ void buildGame(struct Game *currentGame) {
 	for (i = 0; i < 7; i++) {
 		readRoomFile(currentGame, i);
 	}
-	
+	free(fileName);
+	free(roomName);
 }
 
 /******************************************************************************
@@ -160,13 +157,16 @@ void writeRoomFile(struct Game *currentGame, int roomNumber) {
 	sprintf(roomName, "ROOM NAME: %s\n", currentGame->roomList[roomNumber].name);
 	sprintf(roomType, "ROOM TYPE: %s\n", currentGame->roomList[roomNumber].type);
 
+	// open file for writing
 	file_descriptor = open(fileName, O_RDWR);
+	// check to see if file opened successfully
 	if (file_descriptor == -1) {
 		fprintf(stderr, "Could not open %s to write to file.\n", fileName);
 		exit(1);
 	}
-
+	// write the room name to the file
 	nwritten = write(file_descriptor, roomName, strlen(roomName));
+	// write each connecting room name to file
 	for (i = 0; i < 7; i++) {
 		if (currentGame->roomList[roomNumber].connections[i] == 1 && roomNumber != i) {
 			sprintf(connectionRoomName, "CONNECTION %i: %s\n", connectionNumber, currentGame->roomList[i].name);
@@ -174,15 +174,16 @@ void writeRoomFile(struct Game *currentGame, int roomNumber) {
 			connectionNumber++;
 		}
 	}
-
+	// write the room type to the file
 	nwritten = write(file_descriptor, roomType, strlen(roomType));
-
+	// close the file
 	close(file_descriptor);
-
+	// clean up pointers
 	fileName = NULL;
 	roomName = NULL;
 	roomType = NULL;
 	connectionRoomName = NULL;
+	// restore dynamic memory
 	free(fileName);
 	free(roomName);
 	free(roomType);
@@ -205,15 +206,19 @@ void readRoomFile(struct Game *currentGame, int roomNumber) {
 	int count = 0,
 	    i = 0,
 	    index = 0;
+	// concatenate directory path and file name
 	sprintf(fileName, "%s/file%d", currentGame->dirPath, roomNumber);
+	// open file pointer for reading
 	FILE *fp = fopen(fileName, "r");
-	
+	// read each character of the file until the end
 	while ((c = fgetc(fp)) !=  EOF) {
 		if (c == '\n') {
 			buffer[count] = c;
+			// determine which type of row is being read
 			for (i = 0; i < 10; i++) {
 				subBuff[i] = buffer[i];
 			}
+			// store appropriate value based on what the row holds
 			if (strcmp(subBuff, "ROOM NAME:") == 0) {
 				index = 11;
 				count = 0;
@@ -262,16 +267,18 @@ void readRoomFile(struct Game *currentGame, int roomNumber) {
 				}
 				strcpy(currentGame->roomList[roomNumber].type, roomType);
 			}
+			// clear buffer variable for re-use
 			for (i = 0; i < 512; i++) {
 				buffer[i] = '\0';
 			}
 			count = 0;
 		} else {
+			// if not newline, store character and add to total characters read
 			buffer[count] = c;
 			count++;
 		}
 	}
-	
+	// close the file	
 	fclose(fp);
 }
 
@@ -283,15 +290,17 @@ void createRoomFile(struct Game *currentGame, int roomNumber) {
 	char *fileName = malloc(sizeof(char) * 10);
 	int file_descriptor;
 	sprintf(fileName, "%s/file%d", currentGame->dirPath, roomNumber);
+	// open and create file
 	file_descriptor = open(fileName, O_RDONLY | O_CREAT, 0775);
 	if (file_descriptor < 0) {
 		fprintf(stderr, "Could not open %s to create the file.\n", fileName);
 		exit(1);
 	}
-
+	// close the file
 	close(file_descriptor);
-
+	// free pointer
 	fileName = NULL;
+	// free dynamic memory
 	free(fileName);
 }
 
@@ -304,9 +313,11 @@ void addRoomConn(struct Game *currentGame, int roomIndex) {
 	int connectionEstablished = 0,
 		randomSelection = -1;
 	
+	// search for random connection match until established
 	while (connectionEstablished == 0) {
 		// generate random number between 0 and 6
 		randomSelection = (rand() % 7);
+		// add connection if not at max and if connection doesn't already exist
 		if (currentGame->roomList[roomIndex].connections[randomSelection] == 0) {
 				currentGame->roomList[roomIndex].connections[randomSelection] = 1;
 				currentGame->roomList[randomSelection].connections[roomIndex] = 1;
@@ -344,14 +355,21 @@ void getRandomName(struct Game *currentGame, char *name) {
  *   total steps taken and the path traveled from start to finish.
  *****************************************************************************/
 void displayGameResults(struct Game *currentGame) {
-    // display congratulations message
+	FILE *fp = fopen(currentGame->stepFileName, "r");
+	char c;
+	// display congratulations message
     printf("YOU HAVE FOUND THE END ROOM. CONGRATULATIONS!\n");
     // display number of steps taken
     printf("YOU TOOK %i STEPS. ", currentGame->stepCount);
     // display path message
     printf("YOUR PATH TO VICTORY WAS: \n");
+
     // Read path steps from file and display them in order
-    printf("Display steps from file here\n");
+	while ((c = fgetc(fp)) != EOF) {
+		printf("%c", c);
+	}
+
+	fclose(fp);
 }
 
 /******************************************************************************
@@ -363,7 +381,6 @@ void initGame(struct Game *currentGame) {
 	int i = 0,
 		status;
 	char buffer[512];
-	char *fileName = malloc(sizeof(char) * 25);
 	int file_descriptor;
 	
 	// initialize name list with 10 predefined options
@@ -385,7 +402,6 @@ void initGame(struct Game *currentGame) {
 	currentGame->numNamesRemaining = 10;	// number of names available
 	currentGame->startRoomIndex = -1;		// start room index, -1 is bad
 	currentGame->endRoomIndex = -1;			// end room index, -1 is bad
-	currentGame->stepListSize = 10;			// starts with array of 10 strings
 	
 	// randomly select starting room
 	currentGame->startRoomIndex = (rand() % 7);
@@ -402,7 +418,6 @@ void initGame(struct Game *currentGame) {
 		currentGame->roomList[i].connections[i] = 1;
 	}
 
-
 	// gather current process id and build directory path for files
 	currentGame->processID = getpid();		// current process ID for program
 	sprintf(buffer,"gilesm.rooms.%d", currentGame->processID); 
@@ -414,15 +429,13 @@ void initGame(struct Game *currentGame) {
 		createRoomFile(currentGame, i);
 	}
 	// create step list temporary file
-	sprintf(fileName, "%s/stepHistory", currentGame->dirPath);
-	file_descriptor = open(fileName, O_RDONLY | O_CREAT, 0775);
+	sprintf(currentGame->stepFileName, "%s/stepHistory", currentGame->dirPath);
+	file_descriptor = open(currentGame->stepFileName, O_RDONLY | O_CREAT, 0775);
 	if (file_descriptor < 0) {
-		fprintf(stderr, "Could not open %s to create the file.\n", fileName);
+		fprintf(stderr, "Could not open %s to create the file.\n", currentGame->stepFileName);
 		exit(1);
 	}
 	close(file_descriptor);
-	fileName = NULL;
-	free(fileName);
 }
 
 /******************************************************************************
@@ -439,17 +452,21 @@ void playGame(struct Game *currentGame) {
 	char *fileName = malloc(sizeof(char) * 25);
 	char *historyName = malloc(sizeof(char) * 50);
 	ssize_t nwritten;
-
-	sprintf(fileName, "%s/stepHistory", currentGame->dirPath);
-	file_descriptor = open(fileName, O_RDWR);
+	// open step history file for writing
+	file_descriptor = open(currentGame->stepFileName, O_RDWR);
 	if (file_descriptor == -1) {
 		fprintf(stderr, "Could not open %s to write to file.\n", fileName);
 		exit(1);
 	}
-
+	// allow player to move through connected rooms until end room is reached
 	while (currentLocation != currentGame->endRoomIndex) {
+		// allows different formatting for first connection
 		count = 0;
+		// determines if user typed appropriate connection room name
+		success = 0;
+		// shows room name for current room
 		printf("CURRENT LOCATION: %s\n", currentGame->roomList[currentLocation].name);
+		// displays room name for all connected rooms
 		printf("POSSIBLE CONNECTIONS: ");
 		for (i = 0; i < 7; i++) {
 			if (currentGame->roomList[currentLocation].connections[i] == 1 && currentLocation != i) {
@@ -463,22 +480,29 @@ void playGame(struct Game *currentGame) {
 		}
 		printf(".\n");
 		printf("WHERE TO? >");
+		// gets user input for room selection
 		fgets(buffer, 50, stdin);
+		// terminates string with null character
 		buffer[strlen(buffer) - 1] = '\0';
+		// reads through each room to retrieve room names
 		for (i = 0; i < 7; i++) {
 			if (currentGame->roomList[currentLocation].connections[i] == 1 && currentLocation != i) {
 				if (strcmp(currentGame->roomList[i].name, buffer) == 0) {
 					currentLocation = i;
 					sprintf(historyName, "%s\n", currentGame->roomList[i].name);
+					// stores room name for history
 					nwritten = write(file_descriptor, historyName, strlen(historyName));
+					// increments total step count
 					currentGame->stepCount++;
+					// indicates user typed successful connecting room name
 					success = 1;
-					break;
 				}
 			}
 		}
+		printf("\n");
 		if (success == 0) {
 			printf("HUH? I DON'T UNDERSTAND THAT ROOM. TRY AGAIN.\n");
+			printf("\n");
 		}
 	}
 	close(file_descriptor);
